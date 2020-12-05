cfbe7cd2032e2af6ba46ab18dc664a4fa1e13f49
ZhannaBorodaeva <zhborodaeva@gmail.com>
2020-12-05 15:02:10 +0100

    getting ready to push an example cachematrixfile

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Before running makeCacheMatrix I create a random matrix (see comments beloss)
+## To check whether a code works with other matrices, you should redefine
+##a variable ''matrass'' and change ''n'' in ''idenmat'' respectively.
+## My makeCacheMatrix_function uses a randomly generated matrix and creates 
+##variables getglobal, setinvglobal, getinvglobal in the cache.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+n<-sample(2:5,1) #Random choice of columns-rows number as integers from 2 to 5
+print(n)
+matrass<-matrix(rnorm(n*n),n,n) #Random matrix with number of columns-rows of n 
+print(matrass)
+idenmat <- matrix(0, n, n) #Zero matrix of size n
+diag(idenmat) <- 1 #Assigning diagonal elements of the zero matrix value of 1 
+print(idenmat)
+makeCacheMatrix <- function(x = matrass) {
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        } 
+        get <- function() x
+        getglobal<<-get()
+        setinverse<-function() m<<-solve(x)
+        setinvglobal<<-setinverse()
+        getinverse<-function() m
+        getinvglobal<<-getinverse()
 }
-
-
-## Write a short comment describing this function
-
+## My cacheSolve_function at first assigns an inverse matrix (getinversglobal) 
+## and an initial martix (getglobal) to ''m'' and ''data'', respectively.  
+## If non_zero m is an inverse matrix to data than m is retrieved from cache.
+## Alternatively, cacheSolve calculates an inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- getinvglobal
+        data <- getglobal
+        check<-data*m # A variable needed to check whether m is an
+        #inverse matrix to the specific matrix 
+        if(!is.null(m) && check==idenmat) {#If m, i.e. inverse matrix, is not 
+                #zero and multiplication of ''matrass'' and ''m'' equals to 
+                #identity matrix than data are chosen from cache
+                message("getting cached data")
+                return(m)
+        }
+        m <- solve(data)
+        m
 }
+##To check whether the code works, just run it. Firstly, a code will print 
+##number of columns and rows. Secondly, it will print an initial matrix and an
+##identity matrix with relevant number of rows-columns.
+## Then,input makeCacheMatrix()
+## Finally, input cacheSolve() to see an inverse matrix.
+
+
+
+
+
